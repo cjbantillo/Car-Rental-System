8d6d5881a05c644e4c2c97938163c5d04b83806f
cjbantillo <bantillocj1@gmail.com>
2025-01-11 17:18:09 +0800

    add AdminDashboardTemplate component and update routing to use it in AdminDashboardView

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
+// import LoginView from '@/views/LoginView.vue'
+import AdminDashboardTemplate from '@/components/template/AdminDashboard/AdminDashboardTemplate.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: LoginView,
+    // },
     {
       path: '/',
-      name: 'home',
-      component: LoginView,
+      name: 'Dashboard',
+      component: AdminDashboardTemplate,
     },
 
   ],
---
+++ src/views/AdminDashboardView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import adminDashboard from '../components/template/AdminDashboard/AdminDashboardTemplate.vue'
+</script>
+
+<template>
+
+<adminDashboard />
+</template>
