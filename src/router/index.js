53ea1b314c3aad728da6f5acbb12ca1b6c090fe6
cjbantillo <bantillocj1@gmail.com>
2025-01-27 15:24:20 +0800

    feat: add LoginView component and update router with login and register routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/views/AdminDashboardView.vue'
 import cashier from '@/views/CashierDashboard.vue'
+import login from '@/views/LoginView.vue'
+import register from '@/views/RegisterView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,3 +22,14 @@ const router = createRouter({
 })
 
 export default router
+router.addRoute({
+  path: '/login',
+  name: 'login',
+  component: login,
+})
+
+router.addRoute({
+  path: '/register',
+  name: 'register',
+  component: register,
+})
